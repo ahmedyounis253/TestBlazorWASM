74afaa39f547eb86d6b9c0cab69bb655baae9d0d
ahmedyounis253 <amy2532001@gmail.com>
2022-06-05 16:28:43 +0200

    handling client side

--- TestBlazorWASM/TestBlazorWASM/Client/Pages/AddStudent.razor.cs
+++ TestBlazorWASM/TestBlazorWASM/Client/Pages/AddStudent.razor.cs
@@ -1,30 +1,29 @@
 ﻿namespace TestBlazorWASM.Client.Pages
 {
-    public partial class AddEmployee
+    public partial class AddStudent
     {
         [Parameter] public string id { get; set; } 
         [Inject] public HttpClient client { get; set; }
         [Inject] public NavigationManager navigationManager { get; set; }
-        public Employee _employee { get; set; } = new();
+        public Student _student { get; set; } = new();
         protected override  async Task OnParametersSetAsync()
         {
             if (!String.IsNullOrEmpty(id))
-                _employee = await client.GetFromJsonAsync<Employee>($"api/employees/{id}");
-
+                _student = await client.GetFromJsonAsync<Student>($"api/students/{id}");
             else
-                _employee= new Employee();    
+                _student = new Student();
         }
         public async void Submit()
         {
             if (String.IsNullOrEmpty(id))
             {
-                await client.PostAsJsonAsync<Employee>("api/employees", _employee);
-                navigationManager.NavigateTo("employees");
+                await client.PostAsJsonAsync<Student>("api/students", _student);
+                navigationManager.NavigateTo("students");
             }
             else
             {
-                await client.PutAsJsonAsync<Employee>($"api/employees", _employee);
-                navigationManager.NavigateTo("employees");
+                await client.PutAsJsonAsync<Student>($"api/students", _student);
+                navigationManager.NavigateTo("students");
             }
         }
     }
--- TestBlazorWASM/TestBlazorWASM/Client/Pages/Employees.razor.cs
+++ TestBlazorWASM/TestBlazorWASM/Client/Pages/Employees.razor.cs
@@ -4,13 +4,24 @@
     {
         [Inject] public HttpClient client { get; set; }
         [Inject] public NavigationManager navigationManager { get; set; }
+        public string searchText { get; set; }
         List<Employee>? employees;
-        
-        protected override async Task OnInitializedAsync() => employees = await GetEmployees();
+
+        protected override async Task OnInitializedAsync()  {
+            employees = await GetEmployees(); 
+            searchText="";  
+    }
         
         protected override async Task OnParametersSetAsync()
         {
             employees = await GetEmployees();
+            
+            if (!employees.Any())
+            {
+                await LoadDefaultEmployeesAsync();
+                employees = await GetEmployees();
+            }
+
             StateHasChanged();
         }
 
@@ -19,17 +30,30 @@
             return await client.GetFromJsonAsync<List<Employee>>("api/employees");
         }
         
+        public async Task LoadDefaultEmployeesAsync()
+        {
+            FillEmployees fillEmployees = new FillEmployees();
+            List<Employee> emps = fillEmployees.GetEmployees();
+            foreach (Employee employee in emps) await client.PostAsJsonAsync("api/employees", employee);
+
+        }
+
         public void AddEmployee() => navigationManager.NavigateTo("AddEmployee");
         
-        public async void Delete(int id)
+        public async void Delete(Guid? id)
         {
-           await  client.DeleteAsync($"api/employees/{id}");
+           await  client.DeleteAsync($"api/employees/{id.ToString()}");
             employees = await GetEmployees();
             StateHasChanged();
         }
-        public async void Update(int id)
+        public void Update(Guid? id)
         {
-            navigationManager.NavigateTo($"EditEmployee/{id}");
+            navigationManager.NavigateTo($"EditEmployee/{id.ToString()}");
+            StateHasChanged();
+        }
+        public async Task Search()
+        {
+            employees = await client.GetFromJsonAsync<List<Employee>>($"api/employees/Search/{searchText}");
             StateHasChanged();
         }
     }
